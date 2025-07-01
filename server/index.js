5660b455242d8936162dfeb08260d2f545c79cfe
Hardattsinh Zala <zalahardatt@gmail.com>
2025-07-01 16:39:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 const corsOption = {
-    origin: "http://localhost:5173",
+    origin: "https://urbancartel.vercel.app",
     methods: "GET, POST, PATCH, DELETE, PUT, HEAD",
     credentials: true,
 }
