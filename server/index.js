6ee24fa74efce2497807b17fbf7b1adcea83f041
Hardattsinh Zala <zalahardatt@gmail.com>
2025-07-02 22:18:40 +0530

    forgot adding  deployed url

--- server/index.js
+++ server/index.js
@@ -16,8 +16,8 @@ const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 const corsOption = {
-    // origin: "https://urbancartel.vercel.app",
-    origin: "http://localhost:5173",
+    origin: "https://urbancartel.vercel.app",
+    // origin: "http://localhost:5173",
     methods: "GET, POST, PATCH, DELETE, PUT, HEAD",
     credentials: true,
 }
